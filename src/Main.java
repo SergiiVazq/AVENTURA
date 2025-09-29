af5003c996b5f3e20bd3e4ff88159db629135ecc
Sergio Vazquez <sergiovazquezvarela160bc@gmail.com>
2025-09-29 11:50:17 +0200

    cambios finales

--- src/Main.java
+++ src/Main.java
@@ -16,87 +16,92 @@ public class Main {
         nombrearanha = input.nextLine();
 
         System.out.println("\n==========================================================");
-        System.out.println( nombrearanha + "\nComienza la aventura empiezas con 100 de vida intenta sobrevivir en el liceo");
+        System.out.println( nombrearanha + " comienza la aventura empiezas con 100 de vida intenta sobrevivir en el liceo");
         System.out.println("\nSITUACION 1:");
         System.out.println("Pulsa 1 si decides entrar a la clase de DAM por la ventana");
         System.out.println("Pulsa 2 si decides entrar a la clase de DAM por los conductos de ventilacion\n");
         opcion = input.nextInt();
-        if (opcion != 1 || opcion != 2) {
+        if (opcion != 1 && opcion != 2) {
             do {
+                System.out.println("opcion incorrecta pulsa 1 o 2 : ");
                 opcion = input.nextInt();
-            } while (opcion != 1 || opcion != 2);
+            } while (opcion != 1 && opcion != 2);
         }
         if (opcion == 1) {
-            System.out.println( nombrearanha + "Entraste sin complicaciones a la clase bien hecho...");
-            System.out.println("VIDA ACTUAL" + vida);
+            System.out.println( nombrearanha + " Entraste sin complicaciones a la clase bien hecho...");
+            System.out.println("VIDA ACTUAL: " + vida + " ===================");
 
         } else if (opcion == 2) {
             vida -= 25;
             System.out.println( nombrearanha + "Los conductos estaban muy calientes debido al calor y sufriste daño...");
-            System.out.println("VIDA ACTUAL" + vida);
+            System.out.println("VIDA ACTUAL: " + vida + " ===================");
         }
 
         System.out.println("\nSITUACION 2:");
         System.out.println("Pulsa 1 si decides Bajar usando telarañas desde el techo");
         System.out.println("Pulsa 2 si decides Bajar por la pared\n");
         opcion = input.nextInt();
-        if (opcion != 1 || opcion != 2) {
+        if (opcion != 1 && opcion != 2) {
             do {
+                System.out.println("opcion incorrecta pulsa 1 o 2 : ");
                 opcion = input.nextInt();
-            } while (opcion != 1 || opcion != 2);
+            } while (opcion != 1 && opcion != 2);
         }
         if (opcion == 1) {
             vida -= 25;
             System.out.println("Tu telaraña no es muy fuerte y sufres una gran caida...");
-            System.out.println("VIDA ACTUAL" + vida);
+            System.out.println("VIDA ACTUAL: " + vida + " ===================\n");
 
-            System.out.println( nombrearanha + "\nQUE QUIERES HACER AHORA?");
+            System.out.println( nombrearanha + "QUE QUIERES HACER AHORA?");
             System.out.println("Pulsa 1 si decides volver a intentarlo ya que todos en la clase se rieron de ti...");
             System.out.println("Pulsa 2 si decides continuar a la siguiente habitacion\n");
             opcion = input.nextInt();
-            if (opcion != 1 || opcion != 2) {
+            if (opcion != 1 && opcion != 2) {
                 do {
+                    System.out.println("opcion incorrecta pulsa 1 o 2 : ");
                     opcion = input.nextInt();
-                } while (opcion != 1 || opcion != 2);
+                } while (opcion != 1 && opcion != 2);
             }
             if (opcion == 1) {
                 vida -= 25;
-                System.out.println( nombrearanha + "TE VOLVISTE A CAER DE NUEVO!!! QUE VERGUENZA, SIMPLEMENTE AVANZAS A LA SIGUIENTE HABITACIÓN...");
-                System.out.println("VIDA ACTUAL" + vida);
+                System.out.println( nombrearanha + " TE VOLVISTE A CAER DE NUEVO!!! QUE VERGUENZA, SIMPLEMENTE AVANZAS A LA SIGUIENTE HABITACIÓN...");
+                System.out.println("VIDA ACTUAL: " + vida + " ===================\n");
 
             } else if (opcion == 2) {
                 System.out.println("Tardas un poco en avanzar ya que te estas recuperando del golpe...");
-                System.out.println("VIDA ACTUAL" + vida);
+                System.out.println("VIDA ACTUAL: " + vida + " ===================\n");
             }
 
 
         } else if (opcion == 2) {
             System.out.println("Tardaste más pero era la opcion más segura...");
-            System.out.println("VIDA ACTUAL" + vida);
+            System.out.println("VIDA ACTUAL: " + vida + " ===================\n");
         }
 
         System.out.println("\nSITUACION 3:");
         System.out.println("Pulsa 1 si decides entrar al baño...");
         System.out.println("Pulsa 2 si decides ir a la cocina\n");
         opcion = input.nextInt();
-        if (opcion != 1 || opcion != 2) {
+        if (opcion != 1 && opcion != 2) {
             do {
+                System.out.println("opcion incorrecta pulsa 1 o 2 : ");
                 opcion = input.nextInt();
-            } while (opcion != 1 || opcion != 2);
+            } while (opcion != 1 && opcion != 2);
         }
         if (opcion == 1) {
             vida -= 25;
             System.out.println("El suelo esta mojado y recibiste daño, chocaste contra un pie POR POCO TE PISAN !!...");
-            System.out.println("VIDA ACTUAL" + vida);
+            System.out.println("VIDA ACTUAL: " + vida + " ===================\n");
 
             System.out.println("\nSITUACION 4:");
             System.out.println("Pulsa 1 si decides salir por la ventana del baño...");
             System.out.println("Pulsa 2 si decides volver a la clase de DAM y salir por la ventana \n");
             opcion = input.nextInt();
-            if (opcion != 1 || opcion != 2) {
+            if (opcion != 1 && opcion != 2) {
                 do {
+                    System.out.println("opcion incorrecta pulsa 1 o 2 : ");
                     opcion = input.nextInt();
-                } while (opcion != 1 || opcion != 2);
+                } while (opcion != 1 && opcion != 2);
             }
             if (opcion == 1) {
 
